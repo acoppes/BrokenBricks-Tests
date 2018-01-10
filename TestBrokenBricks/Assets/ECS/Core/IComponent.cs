91d3a898abbb80d36876650af3b06cb1f01a3940
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-09 21:54:18 -0300

    removed BrokenBricks code and added submodule

--- TestBrokenBricks/Assets/ECS/Core/IComponent.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace ECS {
-    public interface IComponent {
-    }
-}
--- TestBrokenBricks/Assets/ECS/Unity/VisualDebugging/DebugSystemGroupAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-public class DebugSystemGroupAttribute : Attribute {
-    public readonly string Group;
-    public DebugSystemGroupAttribute(string group) {
-        Group = group;
-    }
-}
