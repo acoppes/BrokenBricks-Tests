9b0ef0020c12c86e5ff3161eefa76606e990e16f
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-14 15:25:51 -0300

    Added more tuple options

--- TestBrokenBricks/Assets/MyTest/InputSystem.cs
+++ TestBrokenBricks/Assets/MyTest/InputSystem.cs
@@ -6,54 +6,6 @@ namespace MyTest.Systems
 {
 	public class InputSystem : ComponentSystem
 	{
-		public struct ComponentTuple {
-
-			ComponentGroup group;
-
-			ComponentArray<InputComponent> inputArray;
-			ComponentArray<ControllerComponent> controllerArray;
-			int _entityIndex;
-
-			public InputComponent inputComponent {
-				get {
-					return inputArray[_entityIndex];
-				} 
-				set {
-					inputArray.GetEntity(_entityIndex).SetComponent(value);
-				}
-			}
-			
-			public ControllerComponent controllerComponent {
-				get {
-					return controllerArray[_entityIndex];
-				} 
-				set {
-					controllerArray.GetEntity(_entityIndex).SetComponent(value);
-				}
-			}
-
-			public int EntityIndex {
-				set {
-					_entityIndex = value;
-				}
-			}
-
-			public int Count {
-				get {
-					return inputArray.Length;
-				}
-			}
-
-			public ComponentTuple(EntityManager manager)
-			{
-				_entityIndex = 0;
-				group = manager.GetComponentGroup(typeof(InputComponent), typeof(ControllerComponent));
-				inputArray = group.GetComponent<InputComponent>();
-				controllerArray = group.GetComponent<ControllerComponent>();
-			}
-
-		}
-
 		[InjectDependency]
 		protected EntityManager _entityManager;
 
