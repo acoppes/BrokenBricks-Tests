8c8e72d00100f1bb5d526f96677438fcb39edc7d
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-14 16:18:12 -0300

    Added a 4 values tuple

--- TestBrokenBricks/Assets/MyTest/ComponentTuples.cs
+++ TestBrokenBricks/Assets/MyTest/ComponentTuples.cs
@@ -115,7 +115,6 @@ namespace Gemserk.ECS
 		where T2 : struct, IComponent
 		where T3 : struct, IComponent {
 
-		// why is the ComponentGroup in the middile of all, I believe it shouldn't be necessary.
 		ComponentGroup group;
 
 		ComponentArray<T1> componentArray1;
@@ -178,6 +177,89 @@ namespace Gemserk.ECS
 			componentArray1 = group.GetComponent<T1>();
 			componentArray2 = group.GetComponent<T2>();
 			componentArray3 = group.GetComponent<T3>();
+		}
+
+	}
+
+	public struct ComponentTuple<T1, T2, T3, T4>
+		where T1 : struct, IComponent 
+		where T2 : struct, IComponent
+		where T3 : struct, IComponent
+		where T4 : struct, IComponent {
+
+		ComponentGroup group;
+
+		ComponentArray<T1> componentArray1;
+		ComponentArray<T2> componentArray2;
+		ComponentArray<T3> componentArray3;
+		ComponentArray<T4> componentArray4;
+
+		int _entityIndex;
+
+		public T1 component1 {
+			get {
+				return componentArray1[_entityIndex];
+			} 
+			set {
+				componentArray1.GetEntity(_entityIndex).SetComponent(value);
+			}
+		}
+		
+		public T2 component2 {
+			get {
+				return componentArray2[_entityIndex];
+			} 
+			set {
+				componentArray2.GetEntity(_entityIndex).SetComponent(value);
+			}
+		}
+
+		public T3 component3 {
+			get {
+				return componentArray3[_entityIndex];
+			} 
+			set {
+				componentArray3.GetEntity(_entityIndex).SetComponent(value);
+			}
+		}
+
+		public T4 component4 {
+			get {
+				return componentArray4[_entityIndex];
+			} 
+			set {
+				componentArray4.GetEntity(_entityIndex).SetComponent(value);
+			}
+		}
+
+		public int EntityIndex {
+			set {
+				_entityIndex = value;
+			}
+		}
+
+		public int Count {
+			get {
+				return componentArray1.Length;
+			}
+		}
+
+        public ComponentGroup Group
+        {
+            get
+            {
+                return group;
+            }
+        }
+
+        public ComponentTuple(EntityManager entityManager)
+		{
+			_entityIndex = 0;
+			group = entityManager.GetComponentGroup(typeof(T1), typeof(T2), typeof(T3), typeof(T4));
+			componentArray1 = group.GetComponent<T1>();
+			componentArray2 = group.GetComponent<T2>();
+			componentArray3 = group.GetComponent<T3>();
+			componentArray4 = group.GetComponent<T4>();
 		}
 
 	}
